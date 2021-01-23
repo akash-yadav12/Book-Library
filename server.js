89c393141811dd8647e34b4e27b74c8b73cb9632
akash <akash.ry@somaiya.edu>
2021-01-23 11:53:46 +0530

    all connection, MVC setup done

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+
 const express = require('express')
 const app = express()
+const indexRouter = require('./routes/index')
 const expressLayouts = require('express-ejs-layouts')
 
 app.set('view engine','ejs')
@@ -9,4 +15,15 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-app.listen(process.env.PORT || 3000)
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+const db = mongoose.connection
+db.on("error", error =>{console.error(error)})
+db.once("open", () =>{console.log("connected to mongoose")})
+
+app.use('/',indexRouter)
+
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log('server Started on port 3000')
+})
